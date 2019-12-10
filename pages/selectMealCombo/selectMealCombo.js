2141ac9895a8d03bbe210397d1101a9ef04c5ba6
deanwang <zabrahair@gmail.com>
2019-12-10 10:30:03 +0800

    add register page design

--- pages/selectMealCombo/selectMealCombo.js
+++ pages/selectMealCombo/selectMealCombo.js
@@ -6,108 +6,118 @@ Page({
    */
   data: {
     total_price: 10000,
-    combo: [
-      {
-        id: '1',
-        name: "主食",
-        options: [
-          { id: '1-1', name: '米饭', isSelect: true ,
-          image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg' },
-          { id: '1-2', name: '炒面', isSelect: false, 
-          image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg' },
-          { id: '1-3', name: '汤面', isSelect: false,
-          image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg' },
-          {
-            id: '1-4', name: '年糕', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-        ]
-      },
-      {
-        id: '2',
-        name: "大荤",
-        options: [
-          {
-            id: '2-1', name: '红烧肉', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '2-2', name: '酱鸭腿', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '2-3', name: '大排骨', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '2-4', name: '咖喱鸡', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-        ]
-      },
-      {
-        id: '3',
-        name: "小荤",
-        options: [
-          {
-            id: '3-1', name: '回锅肉片', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '3-2', name: '番茄炒蛋', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '3-3', name: '肉丝炒韭芽', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-        ]
-      },
-      {
-        id: '4',
-        name: '素菜',
-        options: [
-          {
-            id: '4-1', name: '上汤娃娃菜', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '4-2', name: '干煸草头', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '4-3', name: '土豆烧刀豆', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '4-4', name: '蚝油生菜', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-        ]
-      },
-      {
-        id: '5',
-        name: '汤类',
-        options: [
-          {
-            id: '5-1', name: '肉丝蛋汤', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '5-2', name: '紫菜开洋汤', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '5-3', name: '番茄蛋汤', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-          {
-            id: '5-4', name: '榨菜蛋汤', isSelect: false,
-            image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
-          },
-        ]
-      }
-    ]
+    combo:{
+      id: 'combo-1',
+      name: '小云超值无敌套餐',
+      options: [
+        {
+          id: '1',
+          name: "主食",
+          options: [
+            {
+              id: '1-1', name: '米饭', isSelect: true,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '1-2', name: '炒面', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '1-3', name: '汤面', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '1-4', name: '年糕', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+          ]
+        },
+        {
+          id: '2',
+          name: "大荤",
+          options: [
+            {
+              id: '2-1', name: '红烧肉', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '2-2', name: '酱鸭腿', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '2-3', name: '大排骨', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '2-4', name: '咖喱鸡', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+          ]
+        },
+        {
+          id: '3',
+          name: "小荤",
+          options: [
+            {
+              id: '3-1', name: '回锅肉片', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '3-2', name: '番茄炒蛋', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '3-3', name: '肉丝炒韭芽', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+          ]
+        },
+        {
+          id: '4',
+          name: '素菜',
+          options: [
+            {
+              id: '4-1', name: '上汤娃娃菜', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '4-2', name: '干煸草头', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '4-3', name: '土豆烧刀豆', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '4-4', name: '蚝油生菜', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+          ]
+        },
+        {
+          id: '5',
+          name: '汤类',
+          options: [
+            {
+              id: '5-1', name: '肉丝蛋汤', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '5-2', name: '紫菜开洋汤', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '5-3', name: '番茄蛋汤', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+            {
+              id: '5-4', name: '榨菜蛋汤', isSelect: false,
+              image_url: 'https://ali.xinshipu.cn/20160621_1/original/1466440795346.jpg@288w_216h_99q_1e_1c.jpg'
+            },
+          ]
+        }
+      ]
+    }
   },
 
   /**
